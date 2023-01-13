166686790f87b9ac4d8022d4d49625a4bc906c0a
Chris-Zibra <78902592+Chris-Zibra@users.noreply.github.com>
2023-01-13 16:01:00 +0000

    potacottero

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Quello che sarà Timex.</Text>
+      <Text>POTA</Text>
       <StatusBar style="auto" />
     </View>
   );
