2cad2547fca33166de5b93776ab2aa91b24f6f76
Chris-Zibra <78902592+Chris-Zibra@users.noreply.github.com>
2023-01-13 16:11:06 +0000

    pota 2.0

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>POTA</Text>
+      <Text>POTA ESPLODI UOMO INUTILE</Text>
       <StatusBar style="auto" />
     </View>
   );
