31df2aa6a5dab449e11a7b4f49ac9ea5ba46fb82
WilliamPizzocri <78856766+WilliamPizzocri@users.noreply.github.com>
2023-02-10 10:58:03 +0000

    map added to SearchPage.

--- App.js
+++ App.js
@@ -6,11 +6,40 @@ import { styles } from './style';
 import LandingPage from './screens/LandingPage';
 import ForgetPassword from './screens/ForgetPassword';
 import HomeRouter from './screens/HomeRouter';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      await AsyncStorage.setItem('@geoLatitude', location.coords.latitude.toString());
+      await AsyncStorage.setItem('@geoLongitude', location.coords.longitude.toString());
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   const [fontsLoaded] = useFonts({
     "Roboto-Medium": require('./assets/fonts/Roboto-Medium.ttf'),
--- screens/SearchPage.js
+++ screens/SearchPage.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Dimensions } from "react-native";
+import React, { useState } from "react";
+import MapView from "react-native-maps";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SearchPage = () => {
+  const [latitude, setLatitude] = useState(undefined);
+  const [longitude, setLongitude] = useState(undefined);
+
+  (async () => {
+    const lat = parseFloat(await AsyncStorage.getItem("@geoLatitude"));
+    const long = parseFloat(await AsyncStorage.getItem("@geoLongitude"));
+    setLatitude(lat);
+    setLongitude(long);
+  })();
+
   return (
     <View>
-      <Text>SearchPage</Text>
+      <MapView
+        initialRegion={{
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{
+          width: Dimensions.get('screen').width,
+          height: Dimensions.get('screen').height,
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
